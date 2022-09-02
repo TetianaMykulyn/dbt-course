b83c55e8473e82b6e50b184d348281b1f03ce04d
tanya.mykulyn@gmail.com <tanya.mykulyn@gmail.com>
2022-09-02 10:20:29 +0000

    developing stg_orders

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,22 @@
+select 
+-- from raw orders
+o.OrderId,
+o.OrderDate,
+o.ShipDate,
+o.ShipMode,
+o.OrderSellingPrice - o.OrderCostPrice as OrderProfit,
+o.OrderCostPrice,
+o.OrderSellingPrice,
+-- from raw customer
+c.CustomerName, 
+c.Segment, 
+c.Country, 
+-- from raw product
+p.Category, 
+p.ProductName, 
+p.SubCategory 
+from {{ ref('raw_orders') }} as o
+left join {{ ref('raw_customer') }} as c
+on o.CustomerID = c.CustomerId  
+left join {{ ref('raw_product') }} as p
+on o.ProductID = p.ProductId
